1c066afa6eab3998957ce8c8dd94c3aab701fdb3
GK <gopinath18k@gmail.com>
2025-11-18 18:03:45 +0530

    Geeco Pump All Page Major update Are Done

--- src/components/Home/Header/Navbar.jsx
+++ src/components/Home/Header/Navbar.jsx
@@ -4,7 +4,13 @@ import "./Navbar.css"
 import { IoIosCall,IoIosMail } from "react-icons/io";
 import { IoCloseOutline } from "react-icons/io5";
 import { RxHamburgerMenu } from "react-icons/rx";
-import { Link } from 'react-router-dom';
+import { RiInstagramFill } from "react-icons/ri";
+import { FaLinkedin } from "react-icons/fa6";
+import { FaSquareXTwitter } from "react-icons/fa6";
+import { FaFacebookSquare } from "react-icons/fa";
+
+import { Link, NavLink } from 'react-router-dom';
+
 
 export const Navbar = () => {
 
@@ -14,13 +20,21 @@ export const Navbar = () => {
         setMenuopen(!menuopen)
     }
     const closeMenu=()=>{
-        setMenuopen(!false)
+        setMenuopen(false)
     }
+    React.useEffect(() => {
+    if (menuopen) {
+        document.body.style.overflow = "hidden";
+    } else {
+        document.body.style.overflow = "auto";
+    }
+    }, [menuopen]);
+
 
 
   return (
     <section className="Navbar">
-        <div className="section__Navbar">
+        <div className="section__Navbar" >
             
             <div className="Navbar__container col-6 col-sm-6 col-md-6 col-lg-2">
                 <img src={Logo} alt="" className='logo__image'/>
@@ -30,11 +44,11 @@ export const Navbar = () => {
             </div>
             <div className="Navbar__container Nav_Menus col-10 col-lg-10">
                 <div className="Navbar__Allmenu">
-                    <Link to="/" className="Nav_Home">Home</Link>
-                    <Link to="/all-pumps-categories" className="Nav_All_Pumps">All Pumps</Link>
-                    <Link to="/pump-selection-tool" className="Nav_Pump_Selection_Tool">Pump Selection Tool</Link>
-                    <Link to="/warranty-registration" className="Nav_Warranty_Registration text__menu__red">Warranty Registration</Link>
-                    <Link to="/contact-us" className="Nav_About_Us">Contact Us</Link>
+                    <Link to="/" className="Nav_Home" onClick={() => window.scrollTo(0, 0)}>Home</Link>
+                    <Link to="/all-pumps-categories" className="Nav_All_Pumps" onClick={() => window.scrollTo(0, 0)}>All Pumps</Link>
+                    <Link to="/pump-selection-tool" className="Nav_Pump_Selection_Tool" onClick={() => window.scrollTo(0, 0)}>Pump Selection Tool</Link>
+                    <Link to="/warranty-registration" className="Nav_Warranty_Registration text__menu__red" onClick={() => window.scrollTo(0, 0)}>Warranty Registration</Link>
+                    <Link to="/contact-us" className="Nav_About_Us" onClick={() => window.scrollTo(0, 0)}>Contact Us</Link>
                 </div>     
                
                 <div className="Navbar__contacts col-4">
@@ -51,12 +65,53 @@ export const Navbar = () => {
         </div>
         <div className={menuopen ? "Navbar__MobileMenu active":"Navbar__MobileMenu"}>
             <div className="Navbar__AllMobilemenu">
-                    <Link onClick={closeMenu} to="/" className="Nav_Mobile_Home">Home</Link>
-                    <Link to="/about-us" className="Nav_Mobile_About_Us">About Us</Link>
-                    <Link to="/all-pumps-categories" className="Nav_Mobile_Pumps">All Pumps</Link>
-                    <Link to="/pump-selection-tool" className="Nav_Mobile_Pump_Selection_Tool">Pump Selection Tool</Link>
-                    <Link to="/warranty-registration" className="Nav_Mobile_Warranty_Registration">Warranty Registration</Link>
+                <NavLink 
+                    to="/" 
+                    onClick={() => { closeMenu(); window.scrollTo({ top: 0, behavior: "smooth" }); }} 
+                    className={({ isActive }) => isActive ? "Nav_Mobile_Link active" : "Nav_Mobile_Link"}
+                >
+                    Home
+                </NavLink>
+
+                <NavLink 
+                    to="/all-pumps-categories" 
+                    onClick={() => { closeMenu(); window.scrollTo({ top: 0, behavior: "smooth" }); }} 
+                    className={({ isActive }) => isActive ? "Nav_Mobile_Link active" : "Nav_Mobile_Link"}
+                >
+                    All Pumps
+                </NavLink>
+
+                <NavLink 
+                    to="/pump-selection-tool" 
+                    onClick={() => { closeMenu(); window.scrollTo({ top: 0, behavior: "smooth" }); }} 
+                    className={({ isActive }) => isActive ? "Nav_Mobile_Link active" : "Nav_Mobile_Link"}
+                >
+                    Pump Selection Tool
+                </NavLink>
+
+                <NavLink 
+                    to="/warranty-registration" 
+                    onClick={() => { closeMenu(); window.scrollTo({ top: 0, behavior: "smooth" }); }} 
+                    className={({ isActive }) => isActive ? "Nav_Mobile_Link active" : "Nav_Mobile_Link"}
+                >
+                    Warranty Registration
+                </NavLink>
+
+                <NavLink 
+                    to="/contact-us" 
+                    onClick={() => { closeMenu(); window.scrollTo({ top: 0, behavior: "smooth" }); }} 
+                    className={({ isActive }) => isActive ? "Nav_Mobile_Link active" : "Nav_Mobile_Link"}
+                >
+                    Contact Us
+                </NavLink>
             </div>
+            <div className="Navbar__SocialIcons">
+                <Link><RiInstagramFill className='Navbar_Mobile_Icons'/></Link>
+                <Link><FaLinkedin  className='Navbar_Mobile_Icons'/></Link>
+                <Link><FaFacebookSquare  className='Navbar_Mobile_Icons'/></Link>
+                <Link><FaSquareXTwitter  className='Navbar_Mobile_Icons'/></Link>
+            </div>
+
         </div>
     </section>
   )
